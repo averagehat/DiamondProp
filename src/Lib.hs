72d0b036b6873948bc6cd419893980a371ff2603
Panciera <michael.panciera@amedrhcwrair001.amed.ds.army.mil>
2016-04-21 11:13:34 -0400

    fix syntax error

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,10 @@ module Lib
     ( someFunc
     , diamond
     ) where
+import Data.List (transpose, intercalate)
 
-  where f = (:"")
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+diamond c = intercalate "\n" $ (map f ['A'..c]) ++ (map f $ tail $ reverse ['A'..c])
+  where f = (:"")
